949224aba753e12fdc727191a9970b3fb87fdabf
Diegol29876 <diegol.dg2000@gmail.com>
2026-08-19 11:24:16 -0300

    conexion de BD

--- php/conexion.php
+++ php/conexion.php
@@ -1,8 +1,18 @@
 <?php 
-require_once 'config.php';
-$con = new mysqli(DB_host, DB_usuario, DB_contraseña, DB_nombre);
-if($con->connect_error){
-    die("Error de conexión: " . $con->connect_error);
-}
-echo "Exit en conexion";
+
+    require_once 'config.php';
+
+    function conectar_bd() {
+
+        $con = new mysqli(DB_host, DB_usuario, DB_contraseña, DB_nombre);
+
+        if($con->connect_error){
+            
+            die("Error de conexión: " . $con->connect_error);
+        }
+
+        return $con;
+
+    }
+
 ?>   
--- php/config.php
+++ php/config.php
@@ -1,6 +1,8 @@
 <?php
-define('DB_host', 'localhost');
-define('DB_usuario', 'root');
-define('DB_contraseña', '');
-define('DB_nombre', 'hospitalclinicas');
+
+    define('DB_host', 'localhost');
+    define('DB_usuario', 'root');
+    define('DB_contraseña', '');
+    define('DB_nombre', 'hospitalclinicas');
+    
 ?>
