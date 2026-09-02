bd7e775fe64e8d9a93688cc57242aa8b24a3fb2b
Thiagol10 <dexilirius@gmail.com>
2026-09-02 08:58:52 -0300

    it commit -m "Se agrego la conexion a la base de datos"

--- php/conexion.php
+++ php/conexion.php
@@ -2,7 +2,9 @@
 
     require_once 'config.php';
 
-    function conectar_bd() {
+    function conectar_bd() : mysqli {
+
+    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
         $con = new mysqli(DB_host, DB_usuario, DB_contraseña, DB_nombre);
 
